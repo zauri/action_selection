bc72cdb78cd7957d5e44406b4b5f3c0d4885398d
Zauri <zaurialetarka@gmail.com>
2019-12-03 13:48:49 +0100

    clean up code, update py to working version

--- cognitive model/shortest_path/dijkstra_tum.py
+++ cognitive model/shortest_path/dijkstra_tum.py
@@ -1,4 +1,110 @@
 import pandas as pd
+from scipy.spatial import distance
+
+coordinates = {'c': (1,4),
+              'n': (1,1),
+              'p': (1,4),
+              's': (1,2),
+              't': (1,1),
+              'start': (2,2),
+              'table': (4,3)}
+
+
+def prepare_data(objects_at_once=2):
+    """
+    Reads data from csv and drops node connections (edges) not meeting the specified constraint
+    (e.g. taking more than one object at once).
+    """
+
+    # Read data from csv
+    raw = pd.read_csv('tum_edges_list.csv', header=0, usecols=[0, 1, 2, 3, 4])
+
+    # No constraints for how many objects at once
+    if objects_at_once == 2:
+        data = raw
+    elif objects_at_once == 1:
+        # Add constraint: Only 1 object at once
+        data = raw.loc[raw['obj_at_once'] == 1]
+
+    # Reset index
+    data = data.reset_index(drop=True)
+
+    return data
+
+
+def calculate_distances(data):
+    """
+    Calculates euclidean distance between nodes of graph
+    """
+    for row in range(0, len(data)):
+        # Distance = start -> obj 1 -> table
+        if data['from_node'][row] == 'table_empty' and len(data['to_node'][row]) == 1:
+            data.loc[row, 'dist'] = (distance.euclidean(
+                coordinates['start'],
+                coordinates[data['to_node'][row]]) +
+                                     distance.euclidean(
+                                         coordinates[data['to_node'][row]],
+                                         coordinates['table'])
+                                     )
+
+        # Distance = start -> obj 1 -> obj 2 -> table
+        elif data['from_node'][row] == 'table_empty' and len(data['to_node'][row]) != 1:
+            data.loc[row, 'dist'] = (distance.euclidean(
+                coordinates['start'],
+                coordinates[data['to_node'][row][0]]) +
+                                     distance.euclidean(
+                                         coordinates[data['to_node'][row][0]],
+                                         coordinates[data['to_node'][row][1]]) +
+                                     distance.euclidean(
+                                         coordinates[data['to_node'][row][1]],
+                                         coordinates['table'])
+                                     )
+        # Distance = table -> obj 1 -> (obj 2) -> table
+        else:
+            # Get difference between sequences (from_node, to_node)
+            diff = [x for x in data['to_node'][row] if x not in data['from_node'][row]]
+
+            if len(diff) == 1:
+                data.loc[row, 'dist'] = distance.euclidean(
+                    coordinates['table'],
+                    coordinates[diff[0]]) * 2
+
+            elif len(diff) == 2:
+                data.loc[row, 'dist'] = (distance.euclidean(
+                    coordinates['table'],
+                    coordinates[diff[0]]) +
+                                         distance.euclidean(
+                                             coordinates[diff[0]],
+                                             coordinates[diff[1]]) +
+                                         distance.euclidean(
+                                             coordinates[diff[1]],
+                                             coordinates['table'])
+                                         )
+    return data
+
+
+def calculate_edge_weights(data):
+    """
+    Resets weights according to chosen weight function
+    """
+    for row in range(0, len(data)):
+        if 'c' in data['to_node'][row]:
+            data.loc[row, 'weight_new'] = (data['dist'][row] ** 1.0) * 1.2
+        elif 'p' in data['to_node'][row]:
+            data.loc[row, 'weight_new'] = (data['dist'][row] ** 0.9) * 1.2
+        elif 's' in data['to_node'][row]:
+            data.loc[row, 'weight_new'] = (data['dist'][row] ** 1.0) * 1.2
+        elif 'n' in data['to_node'][row]:
+            data.loc[row, 'weight_new'] = (data['dist'][row] ** 0.95) * 1.0
+        elif 't' in data['to_node'][row]:
+            data.loc[row, 'weight_new'] = (data['dist'][row] ** 0.9) * 1.0
+        else:
+            data.loc[row, 'weight_new'] = data['dist'][row]
+
+    for row in range(0, len(data)):
+        data.loc[row, 'weight_rounded'] = round(data['weight_new'][row], 2)
+
+    return data
 
 
 class Node:
@@ -91,47 +197,10 @@ def to_array(prev, from_node):
     return route
 
 
-# Prepare data
-def prepare_data(objects_at_once=2):
-    """Reads data from csv and drops node connections (edges) not meeting the specified constraint
-    (e.g. taking more than one object at once)."""
-
-    # Read data from csv
-    raw = pd.read_csv('tum_edges_list.csv', header=0, usecols=[0, 1, 2, 3, 4])
-
-    # No constraints for how many objects at once
-    if objects_at_once == 2:
-        data = raw
-    elif objects_at_once == 1:
-        # Add constraint: Only 1 object at once
-        data = raw.loc[raw['obj_at_once'] == 1]
-
-    # Reset index
-    data = data.reset_index(drop=True)
-
-    return data
-
-
-def calculate_edge_weights(data):
-    # Reset weights according to weight function
-    for row in range(0, len(data)):
-        if 'c' in data['to_node'][row]:
-            data.loc[row, 'weight_new'] = (data['weight'][row] ** 1.0) * 1.2
-        elif 'p' in data['to_node'][row]:
-            data.loc[row, 'weight_new'] = (data['weight'][row] ** 0.9) * 1.2
-        elif 's' in data['to_node'][row]:
-            data.loc[row, 'weight_new'] = (data['weight'][row] ** 1.0) * 1.2
-        elif 'n' in data['to_node'][row]:
-            data.loc[row, 'weight_new'] = (data['weight'][row] ** 0.95) * 1.0
-        elif 't' in data['to_node'][row]:
-            data.loc[row, 'weight_new'] = (data['weight'][row] ** 0.9) * 1.0
-        else:
-            data.loc[row, 'weight_new'] = data['weight'][row]
-
-    return data
-
-
 def create_nodes_edges(graph, data):
+    """
+    Creates nodes and edges for graph from data
+    """
     # Create sorted set of nodes from to_nodes and from_nodes
     nodes = sorted(pd.unique(data[['from_node', 'to_node']].values.ravel()))
 
@@ -154,14 +223,15 @@ def print_result(dist, prev):
         table_empty.label,
         pcstn.label,
         " -> ".join(to_array(prev, pcstn)),
-        str(round(dist[pcstn], 1))
+        str(round(dist[pcstn], 2))
         )
     )
 
 
 def main():
     graph = Graph()
-    data = prepare_data(objects_at_once=2)
+    data = prepare_data(objects_at_once=1)
+    data = calculate_distances(data)
     data = calculate_edge_weights(data)
     create_nodes_edges(graph, data)
     dist, prev = dijkstra(graph, table_empty)
