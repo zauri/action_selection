06575c0ba1a74eaad6a0c91d9d932568c7029f4f
Zauri <zaurialetarka@gmail.com>
2020-01-31 11:38:21 +0100

    update plots for figures + figures

--- cognitive model/shortest_path/EPIC/EPIC_plot_modelfit_normalized.R
+++ cognitive model/shortest_path/EPIC/EPIC_plot_modelfit_normalized.R
@@ -4,25 +4,25 @@ model_ck <- c(0.33,0.6,0.25,0.5,0,0,0,0.43,0.25,0.29,0.33,0.78,0.8,0,0.4,0.4)
 k_set <- c(0.33,0.6,0.25,0.5,0,0.33,0,0.71,0.25,0.29,0.33,0.78,0.8,0,0.4,0.4)
 c_set <- c(0.67,0.6,0.25,0.5,0,0,0,0.57,0.5,0.43,0.67,0.78,0.8,0.5,0.4,0.6)
 no_params <- c(0.67,0.6,0.75,1,0,0.33,0.2,0.86,0.5,0.43,0.67,1,0.8,0.5,0.4,0.6)
-avg <- c(0.44,0.668,0.605,0.605,0.44,0.44,0.668,0.756,0.605,0.756,0.725,0.80,0.668,0.25,0.668,0.668)
+avg <- c(0.44,0.666,0.605,0.605,0.44,0.44,0.666,0.756,0.605,0.756,0.723,0.806,0.666,0.25,0.666,0.666)
 
 # Plot model, c+k set
-plot(xdata, model_ck, type='o', col='blue', pch=15, lty=2, ylim=c(0,1.5), lwd=2, 
+plot(xdata, model_ck, type='o', col='blue', pch=15, lty=3, ylim=c(0,1.2), lwd=2, 
      ylab='Normalized Damerau-Levenshtein distance',
      xlab='Video sequence', las=1, xaxt='n', cex.lab=1.4)
 axis(side=1, at=1:16, labels=xdata, cex.axis=1)
   
 # Plot model without c
-points(xdata, k_set, col='red', pch=16)
-lines(xdata, k_set, col='red', lty=1, lwd=2)
+points(xdata, k_set, col='green3', pch=16)
+lines(xdata, k_set, col='green3', lty=1, lwd=2)
 
 # Plot model without k
-points(xdata, c_set, col='dark green', pch=17)
-lines(xdata, c_set, col='dark green', lty=1, lwd=2)
+points(xdata, c_set, col='darkorange1', pch=17)
+lines(xdata, c_set, col='darkorange1', lty=1, lwd=2)
 
 # Plot greedy without params
 points(xdata, no_params, col='dark magenta', pch=18)
-lines(xdata, no_params, col='dark magenta', lty=1, lwd=2)
+lines(xdata, no_params, col='dark magenta', lty=3, lwd=2)
 
 # Plot avg/baseline
 points(xdata, avg, col='black', pch=20)
@@ -30,9 +30,13 @@ lines(xdata, avg, col='black', lty=2, lwd=2)
 
 # Plot model
 points(xdata, model_ck, col='blue', pch=15)
-lines(xdata, model_ck, col='blue', lty=2, lwd=2)
+lines(xdata, model_ck, col='blue', lty=3, lwd=2)
 
-legend(11.8,1.53,legend=c('c+k set', 'k set', 'c set', 'no parameters set','mean edit distance'), 
-       col=c('blue', 'red', 'dark green', 'dark magenta','black'), 
-       pch=c(15,16,17,18,20), lty=c(1,1,1,1,2), ncol=1, cex=1.2)
+
+# Plot legend in 3 columns
+legend_order <- matrix(1:6, ncol=3, byrow=TRUE)
+
+legend(2,1.23, c('c+k set', 'k set', 'c set', 'no parameters set','mean edit distance')[legend_order], 
+       col=c('blue', 'green3', 'darkorange1', 'dark magenta', 'black')[legend_order], 
+       pch=c(15,16,17,18,20)[legend_order], lty=c(3,1,1,3,2)[legend_order], ncol=3, cex=1.2)
 
--- cognitive model/shortest_path/tum_plot_modelfit.R
+++ cognitive model/shortest_path/tum_plot_modelfit.R
@@ -4,10 +4,11 @@ model_ck <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0.17,0.17,0,0,0.17,0.17)
 k_set <- c(0.8,0.8,0,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0.8,0,0.8,0.8,0.8,0.8,0.8,0.8,0.8)
 c_set <- c(0.4,0.4,0,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0,0.4,0.4,0.4,0.4,0.4,0.4,0.4)
 no_params <- c(1,1,0.67,1,1,1,1,1,1,1,1,0.67,1,0.8,0.8,1,1,0.8,0.8)
-avg <- c(0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668,0.668)
+globally_optimal <- c(0.4,0.4,0.67,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.67,0.4,0.33,0.33,0.4,0.4,0.33,0.33)
+avg <- c(0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666)
 
 # Plot model, c+k set
-plot(xdata, model_ck, type='o', col='blue', pch=15, lty=1, ylim=c(0,1.5), lwd=2, 
+plot(xdata, model_ck, type='o', col='blue', pch=15, lty=1, ylim=c(0,1.2), lwd=2, 
      ylab='Normalized Damerau-Levenshtein distance',
      xlab='Video sequence', las=1, xaxt='n', cex.lab=1.4)
 axis(side=1, at=1:19, labels=xdata, cex.axis=1)
@@ -28,6 +29,13 @@ lines(xdata, no_params, col='dark magenta', lty=1, lwd=2)
 points(xdata, avg, col='black', pch=20)
 lines(xdata, avg, col='black', lty=2, lwd=2)
 
-legend(14,1.53,legend=c('c+k set', 'k set', 'c set', 'no parameters set','mean edit distance'), 
-       col=c('blue', 'red', 'dark green', 'dark magenta','black'), 
-       pch=c(15,16,17,18,20), lty=c(1,1,1,1,2), ncol=1, cex=1.2)
+# Plot globally optimal/shortest path
+points(xdata, globally_optimal, col='darkorange1', pch=21)
+lines(xdata, globally_optimal, col='darkorange1', lty=2, lwd=2)
+
+# Plot legend in 3 columns
+legend_order <- matrix(1:6, ncol=3, byrow=TRUE)
+
+legend(2,1.23, c('c+k set','k set','c set','no parameters set','mean edit distance','global shortest path')[legend_order], 
+       col=c('blue','red','dark green','dark magenta','black','darkorange1')[legend_order], 
+       pch=c(15,16,17,18,20,21)[legend_order], lty=c(1,1,1,1,2,2)[legend_order], ncol=3, cex=1.2)
