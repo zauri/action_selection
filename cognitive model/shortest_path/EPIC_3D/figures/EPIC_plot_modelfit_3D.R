e8ab6de586313c12f1677a18991bafc8f2d97a21
Zauri <zaurialetarka@gmail.com>
2020-03-02 13:37:09 +0100

    add 3D analysis for TUM + EPIC

---
+++ cognitive model/shortest_path/EPIC_3D/figures/EPIC_plot_modelfit_3D.R
@@ -0,0 +1,42 @@
+# Define data (x: videos, y: fit of model, edit distance with Damerau-Levenshtein)
+xdata <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+model_ck <- c(0.33,0.6,0.25,0.5,0,0,0,0.43,0.25,0.29,0.33,0.78,0.8,0,0.4,0.4)
+k_set <- c(0.33,0.6,0.25,0.5,0,0.33,0,0.71,0.25,0.29,0.33,0.78,0.8,0,0.4,0.4)
+c_set <- c(0.67,0.6,0.25,0.5,0,0,0,0.57,0.5,0.43,0.67,0.78,0.8,0.5,0.4,0.6)
+no_params <- c(0.67,0.6,0.75,1,0,0.33,0.2,0.86,0.5,0.43,0.67,1,0.8,0.5,0.4,0.6)
+avg <- c(0.44,0.666,0.605,0.605,0.44,0.44,0.666,0.756,0.605,0.756,0.723,0.806,0.666,0.25,0.666,0.666)
+
+# Plot model, c+k set
+plot(xdata, model_ck, type='o', col='blue', pch=15, lty=3, ylim=c(0,1.2), lwd=2, 
+     ylab='Normalized Damerau-Levenshtein distance',
+     xlab='Video sequence', las=1, xaxt='n', cex.lab=1.4)
+axis(side=1, at=1:16, labels=xdata, cex.axis=1)
+  
+# Plot model without c
+points(xdata, k_set, col='green3', pch=16)
+lines(xdata, k_set, col='green3', lty=1, lwd=2)
+
+# Plot model without k
+points(xdata, c_set, col='darkorange1', pch=17)
+lines(xdata, c_set, col='darkorange1', lty=1, lwd=2)
+
+# Plot greedy without params
+points(xdata, no_params, col='dark magenta', pch=18)
+lines(xdata, no_params, col='dark magenta', lty=3, lwd=2)
+
+# Plot avg/baseline
+points(xdata, avg, col='black', pch=20)
+lines(xdata, avg, col='black', lty=2, lwd=2)
+
+# Plot model
+points(xdata, model_ck, col='blue', pch=15)
+lines(xdata, model_ck, col='blue', lty=3, lwd=2)
+
+
+# Plot legend in 3 columns
+legend_order <- matrix(1:6, ncol=3, byrow=TRUE)
+
+legend(2,1.23, c('c+k set', 'k set', 'c set', 'no parameters set','mean edit distance')[legend_order], 
+       col=c('blue', 'green3', 'darkorange1', 'dark magenta', 'black')[legend_order], 
+       pch=c(15,16,17,18,20)[legend_order], lty=c(3,1,1,3,2)[legend_order], ncol=3, cex=1.2)
+
---
+++ cognitive model/shortest_path/TUM_3D/figures/tum_plot_modelfit_3D.R
@@ -0,0 +1,41 @@
+# Define data (x: videos, y: fit of model, edit distance with Damerau-Levenshtein)
+xdata <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
+model_ck <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0.2,0.2,0,0,0.2,0.2)
+k_set <- c(0.2,0.2,0,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0,0.2,0.4,0.4,0.2,0.2,0.4,0.4)
+c_set <- c(0.4,0.4,0.67,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.67,0.4,0.4,0.4,0.4,0.4,0.4,0.4)
+no_params <- c(0.4,0.4,0.67,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.67,0.4,0.4,0.4,0.4,0.4,0.4,0.4)
+globally_optimal <- c(0.4,0.4,0.67,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.67,0.4,0.33,0.33,0.4,0.4,0.33,0.33)
+avg <- c(0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666)
+
+# Plot model, c+k set
+plot(xdata, model_ck, type='o', col='blue', pch=15, lty=1, ylim=c(0,0.8), lwd=2, 
+     ylab='Normalized Damerau-Levenshtein distance',
+     xlab='Video sequence', las=1, xaxt='n', cex.lab=1.4)
+axis(side=1, at=1:19, labels=xdata, cex.axis=1)
+
+# Plot model without c
+points(xdata, k_set, col='red', pch=16)
+lines(xdata, k_set, col='red', lty=1, lwd=2)
+
+# Plot model without k
+points(xdata, c_set, col='dark green', pch=17)
+lines(xdata, c_set, col='dark green', lty=1, lwd=2)
+
+# Plot greedy without params
+#points(xdata, no_params, col='dark magenta', pch=18)
+#lines(xdata, no_params, col='dark magenta', lty=1, lwd=2)
+
+# Plot avg/baseline
+points(xdata, avg, col='black', pch=20)
+lines(xdata, avg, col='black', lty=2, lwd=2)
+
+# Plot globally optimal/shortest path
+points(xdata, globally_optimal, col='darkorange1', pch=21)
+lines(xdata, globally_optimal, col='darkorange1', lty=2, lwd=2)
+
+# Plot legend in 3 columns
+legend_order <- matrix(1:6, ncol=3, byrow=TRUE)
+
+legend(2,0.83, c('c+k set','k set','c set','no parameters set','mean edit distance','global shortest path')[legend_order], 
+       col=c('blue','red','dark green','dark magenta','black','darkorange1')[legend_order], 
+       pch=c(15,16,17,18,20,21)[legend_order], lty=c(1,1,1,1,2,2)[legend_order], ncol=3, cex=1.2)
