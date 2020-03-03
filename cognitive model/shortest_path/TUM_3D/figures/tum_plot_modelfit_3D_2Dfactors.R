470b14176a3493c9aeb87ef870285e480897e939
Zauri <zaurialetarka@gmail.com>
2020-03-03 14:33:08 +0100

    update model factors, new graphs

---
+++ cognitive model/shortest_path/TUM_3D/figures/tum_plot_modelfit_3D_2Dfactors.R
@@ -0,0 +1,50 @@
+library(RColorBrewer)
+
+# Define data (x: videos, y: fit of model, edit distance with Damerau-Levenshtein)
+xdata <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
+model_ck <- c(0.2,0.2,0,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0,0.2,0.4,0.4,0.2,0.2,0,0)
+k_set <- c(0.2,0.2,0.33,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.33,0.2,0.4,0.4,0.2,0.2,0,0)
+c_set <- c(0.2,0.2,0.33,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.2,0.33,0.2,0.4,0.4,0.2,0.2,0,0)
+no_params <- c(0.4,0.4,0.33,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.33,0.4,0.4,0.4,0.4,0.4,0.2,0.2)
+globally_optimal <- c(0.6,0.6,0.67,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.67,0.6,0.5,0.5,0.6,0.6,0.33,0.33)
+avg <- c(0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666,0.666)
+
+col = brewer.pal(n=6,name='Set1')
+
+# Plot model, c+k set
+plot(xdata, model_ck, type='o', col=col[1], pch=15, lty=1, ylim=c(0,0.8), lwd=2, 
+     ylab='Normalized Damerau-Levenshtein distance', main="TUM, 3D model",
+     xlab='Video sequence', las=1, xaxt='n', cex.lab=1.4)
+axis(side=1, at=1:19, labels=xdata, cex.axis=1)
+
+# Plot model without c
+points(xdata, k_set, col=col[2], pch=16)
+lines(xdata, k_set, col=col[2], lty=3, lwd=2)
+
+# Plot model without k
+points(xdata, c_set, col=col[3], pch=17)
+lines(xdata, c_set, col=col[3], lty=2, lwd=2)
+
+# Plot greedy without params
+points(xdata, no_params, col=col[4], pch=18)
+lines(xdata, no_params, col=col[4], lty=2, lwd=2)
+
+# Plot avg/baseline
+points(xdata, avg, col='black', pch=20)
+lines(xdata, avg, col='black', lty=2, lwd=2)
+
+# Plot globally optimal/shortest path
+points(xdata, globally_optimal, col=col[5], pch=21)
+lines(xdata, globally_optimal, col=col[5], lty=2, lwd=2)
+
+# Plot model again for visibility
+points(xdata, model_ck, col=col[1], pch=15)
+lines(xdata, model_ck, col=col[1], lty=2, lwd=2)
+
+
+# Plot legend in 3 columns
+legend_order <- matrix(1:6, ncol=3, byrow=TRUE)
+
+legend(2,0.83, c('c+k set','k set','c set','no parameters set','mean edit distance','global shortest path')[legend_order], 
+       col=c(col[1],col[2],col[3],col[4],'black',col[5])[legend_order], 
+       pch=c(15,16,17,18,20,21)[legend_order], lty=c(1,1,1,1,2,2)[legend_order], ncol=3, cex=1.2)
