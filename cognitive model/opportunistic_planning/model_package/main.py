c65435b3ff055dbe7d22e1abc6c47fd9e6a72a21
zauri <zaurialetarka@gmail.com>
2021-05-12 11:20:03 +0200

    update testing script

--- cognitive model/opportunistic_planning/model_package/main.py
+++ cognitive model/opportunistic_planning/model_package/main.py
@@ -11,11 +11,12 @@ from opportunistic_planning import processing, prediction
 from scipy.stats import friedmanchisquare, wilcoxon
 
 data = pd.read_csv('all_task_environments_2021-03-17.csv', header=0)
+#data = data[:10]
 
 distances_dict = processing.generate_distances_dict(data)
 
 results = processing.calculate_prediction_error(data, distances_dict, error_function='prequential',
           n=10)
 
-#lowest_mean, lowest_mean_idx, lowest_median, results = data.get_lowest_error(results)
+lowest_mean, lowest_mean_idx, lowest_median, results_mean = processing.get_lowest_error(results)
 
